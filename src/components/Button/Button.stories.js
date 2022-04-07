c7fab07ece527982add24af2d3b0b2c62e5519be
ranja <seolin9340@gmail.com>
2022-04-07 19:28:05 +0900

    [Update] Template TIL

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,9 +4,26 @@ import Button from "./Button";
 export default {
 	title: "Form/Button",
 	component: Button,
+	args: {
+		children: "Button",
+	},
 };
+
+const Template = (args) => <Button {...args} />;
 
 export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Secondary = () => <Button variant='secondary'>Secondary</Button>;
 export const Success = () => <Button variant='success'>Success</Button>;
 export const Danger = () => <Button variant='danger'>Danger</Button>;
+
+export const PrimaryA = Template.bind({});
+PrimaryA.args = {
+	variant: "primary",
+	// children: "Primary Args",
+};
+
+export const LongPrimaryA = Template.bind({});
+LongPrimaryA.args = {
+	...PrimaryA.args,
+	// children: "Long Primary Args",
+};
